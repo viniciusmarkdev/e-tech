e5462dc193b3f6d35ec4e1d2ff1ce7d087049226
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-06-28 18:19:34 -0300

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RoutingAppModule } from './routing-app.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RoutingAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
