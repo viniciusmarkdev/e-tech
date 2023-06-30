a5141e81534a28c8686025ffa6ad62bc862c9886
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-06-30 19:04:13 -0300

    Criação da página de produto

---
+++ src/app/produtos.ts
@@ -0,0 +1,43 @@
+export interface IProduto{
+
+    id:number;
+    descricao:string;
+    preco:number;
+    descricaoPreco:string;
+    imagem:string;
+    
+}
+
+export const produtos = [
+  
+     {id:1, descricao: "Mouse gamer" , preco : 439.00 , descricaoPreco : "À vista no PIX" , imagem:"/assets/mouse-3.jpg"},
+     {id:2, descricao: "Monitor muito bom  " , preco : 1200.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/monitor-1.jpg"},
+     {id:3, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/fone-de-ouvido-1.jpg"},
+     {id:4, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/fone-de-ouvido-2.jpg"},
+     {id:5, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/fone-de-ouvido-3.jpg"},
+     {id:6, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/hd.jpg"},
+     {id:7, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/laptop-1.jpg"},
+     {id:8, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/laptop-2.jpg"},
+     {id:9, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/mouse-1.png"},
+     {id:10, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/mouse-2.jpg"},
+     {id:11, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/mouse-4.jpg"},
+     {id:12, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/placa-video.jpg"},
+     {id:13, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/processador.jpg"},
+     {id:14, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/teclado-1.jpg"},
+     {id:15, descricao: " Teclado excelente  " , preco : 749.00 , descricaoPreco : "À vista no PIX" , desconto: 2000.00,
+     imagem:"/assets/teclado-2.jpg"},
+
+]
